d01b67bb36157f88d638164b25689cd3a58db4b5
tigertonyuk <anthonyearpmail@gmail.com>
2026-02-08 09:33:57 +0000

    Delete src/process directory

--- src/process/pdf.js
+++
@@ -1,7 +0,0 @@
-import fs from "fs/promises"; import { PDFDocument } from "pdf-lib";
-export async function brandPdf({inputPath,logoPath,outputPath}){
-  const pdf=await PDFDocument.load(await fs.readFile(inputPath));
-  const logo=await pdf.embedPng(await fs.readFile(logoPath));
-  for(const p of pdf.getPages()){ const {height}=p.getSize(); p.drawImage(logo,{x:20,y:height-100,width:80,height:40}); }
-  await fs.writeFile(outputPath, await pdf.save());
-}
